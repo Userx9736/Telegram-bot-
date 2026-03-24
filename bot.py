59e8232735ddcc8f8674df674b3cd18470fb8d5a
Userx9736 <usernameusernamr152@gmail.com>
2026-03-24 11:36:11 +0530

    Implement Telegram bot to download videos

---
+++ bot.py
@@ -0,0 +1,29 @@
+import yt_dlp
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+BOT_TOKEN = "8594654099:AAHc6iXGsVrhLEOta77GETRxYy6c5yhVq1k
+
+async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    await update.message.reply_text("Downloading...")
+
+    ydl_opts = {
+        'outtmpl': 'video.%(ext)s',
+        'quiet': True
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        await update.message.reply_video(video=open('video.mp4', 'rb'))
+
+    except:
+        await update.message.reply_text("Failed to download")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, handle))
+
+app.run_polling()
